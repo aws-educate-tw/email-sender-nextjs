59079bec6437579b43b0f26c32bb85ae573359cd
chungchihhan <harryup2000@gmail.com>
2024-04-25 23:40:34 +0800

    style: change the UI of the main page and the sendEmail page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,44 @@
-"use client";
-import React from "react";
-import SendForm from "@/app/ui/send-form";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Page() {
   return (
-    <div>
-      <SendForm />
-    </div>
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-sky-950 p-4 md:h-52">
+        <Image
+          src="/aws-educate-logo.png"
+          alt="the logo of aws educate"
+          width={600}
+          height={500}
+          className="w-60 sm:w-60 md:w-96"
+        />
+      </div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-4/5 md:px-20">
+          <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
+            <strong>Welcome to AWS Educate.</strong>
+            <br />
+            <span className="text-lg">
+              This is the TPET for AWS Ambassadors
+            </span>
+          </p>
+          <Link
+            href="/sendEmail"
+            className="flex items-center gap-5 self-start rounded-lg bg-sky-950 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-sky-900 md:text-base"
+          >
+            I want to send emails.
+          </Link>
+        </div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-12 md:py-4">
+          <Image
+            src="/aws-educate-6th.jpg"
+            width={1200}
+            height={1000}
+            className="hidden md:block"
+            alt="6th-aws-educate"
+          />
+        </div>
+      </div>
+    </main>
   );
 }
--- src/app/ui/send-form.tsx
+++ src/app/ui/send-form.tsx
@@ -15,7 +15,7 @@ export default function SendForm() {
             name="email_title"
             type="text"
             placeholder="email_title"
-            className="block rounded-md border border-gray-200 py-2 pl-4 text-sm outline-2 placeholder:text-gray-500"
+            className="block rounded-md border border-gray-200 py-2 pl-4 text-sm outline-2 placeholder:text-gray-500 w-full"
           />
         </div>
         <div className="m-3">
@@ -27,7 +27,7 @@ export default function SendForm() {
             name="template_file_id"
             type="text"
             placeholder="template_file_id"
-            className="block rounded-md border border-gray-200 py-2 pl-4 text-sm outline-2 placeholder:text-gray-500"
+            className="block rounded-md border border-gray-200 py-2 pl-4 text-sm outline-2 placeholder:text-gray-500 w-full"
           />
         </div>
         <div className="m-3">
@@ -39,17 +39,23 @@ export default function SendForm() {
             name="spreadsheet_id"
             type="text"
             placeholder="spreadsheet_id"
-            className="block rounded-md border border-gray-200 py-2 pl-4 text-sm outline-2 placeholder:text-gray-500"
+            className="block rounded-md border border-gray-200 py-2 pl-4 text-sm outline-2 placeholder:text-gray-500 w-full"
           />
         </div>
-        <div className="m-3">
-          <button
-            type="submit"
-            className="flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50"
-          >
-            Send Form
-          </button>
-        </div>
+      </div>
+      <div className="w-full flex justify-end my-3 gap-3">
+        <button
+          type="submit"
+          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="flex h-10 items-center rounded-lg bg-sky-800 px-4 text-sm font-medium text-white transition-colors hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-800 active:bg-sky-950 aria-disabled:cursor-not-allowed aria-disabled:opacity-50"
+        >
+          Send Form
+        </button>
       </div>
     </form>
   );
