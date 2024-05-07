8fde3da1226b071836b29efc67eeafb23ddffec6
chungchihhan <harryup2000@gmail.com>
2024-05-07 14:09:36 +0800

    move the css into the html tag

--- src/app/htmlTemplate/page.tsx
+++ src/app/htmlTemplate/page.tsx
@@ -83,118 +83,58 @@ export default function Home() {
     bannerImageUrl: string,
     avatarImageUrl: string
   ) => `
-  <!DOCTYPE html>
-  <html lang="zh-TW">
-  <head>
-      <meta charset="UTF-8">
-      <title>${title}</title>
-      <style>
-          body {
-              font-family: 'Arial', sans-serif;
-              background-color: #f4f4f9;
-              margin: 0;
-              padding: 20px;
-          }
-          .container {
-              background-color: white;
-              padding-top: 40px;
-              padding-bottom: 60px;
-              padding-left: 60px;
-              padding-right: 60px;
-              margin: auto;
-              max-width: 800px;
-              border-radius: 8px;
-              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-          }
-          p {
-              line-height: 1.6;
-          }
-          a {
-              color: #0077cc;
-          }
-          ul {
-              padding-left: 20px;
-          }
-          .banner {
-              width: 100%;
-              margin-bottom: 10px;
-              border-radius: 8px;
-          }
-          .footer {
-              display: flex;
-              align-items: center;
-              text-align: start;
-              font-size: 0.9em;
-              color: #777;
-          }
-          .avatar {
-              width: 60px; 
-              height: 60px; 
-              border-radius: 50%;
-              border: solid;
-              border-width: 2px ;
-              border-color: #333;
-              margin-right: 15px; 
-          }
-          .avatar-info p {
-              display: flex;
-              flex-direction: column;
-              line-height: 0.5 ;
-          }
-          .important {
-              color: red;
-          }
-          .highlight {
-              color: #FB2200;
-              font-weight: bold;
-          }
-      </style>
-  </head>
-  <body>
-      <div class="container">
-          <img class="banner" src="${
-            bannerImageUrl || "/aws-educate-banner.png"
-          }" alt="Banner of AWS Educate">
-          <p>親愛的{name} ，</p>
-          <div>
-              <p>您好！</p>
-              <p>恭喜您成功加入<strong class="highlight">「${title}」</strong>！非常高興您決定與我們一起踏上學習的旅程，共同探索雲端技術的無限可能！</p>
-              <p>為確保您能夠有效利用本計畫資源，校園大使團隊精心統整許多考照資源於在 Notion Page，並建立 Discord 社群以促進更深入的討論和即時互動，讓您可以輕鬆地提問並與大使互動問答。</p>
-              <b>您可以透過以下連結訪問：</b>
-              <ul>
-                <li>Notion Page：<a target="_blank" href="https://aws-educate-tw.notion.site/AWS-0824fda6e4aa470e863c4d91daf9563a">點擊這裡</a></li>
-                <li>Discord 社群：{Discord Link}</li>
-              </ul>
-          </div>
-          <b>【注意事項】</b>
-          <ul>
-              <li>請記得定期追蹤計畫 Notion Page 和 Discord 社群，我們會定期更新資源和消息。</li>
-              <li>陪跑計畫包含證照課程與獎勵課程，獎勵課程需完成規定的課程進度才可以參與，詳細規則請見 Notion Page。</li>
-          </ul>
-          <b>【開幕活動與報名資訊】</b>
-          <div>
-              <p>隨著「${title}」正式啟動，${content}</p>
-              <p>有意願參加活動請填寫以下報名表單，<span class="important">表單將於 ${deadline} 截止</span>：<a target="_blank" href="${eventLink}">點擊這裡</a></p>
-              <p>最後，我們再次感謝您的參與。這是一場充滿學習和成長的旅程，我們期待在開幕活動上見到您！</p>
-              <p>若您有任何問題或需要進一步的幫助，請聯繫我們。</p>
-              <p>台灣 AWS Educate Cloud Ambassador 官方社群：<a target="_blank" href="https://bit.ly/3pD9aCr">Facebook</a>＆<a target="_blank" href="https://bit.ly/3BBr7XQ">Instagram</a></p>
-          </div>
-                  
-          <p>Best regards,</p>
-          <div class="footer">
-              <img class="avatar" src="${
-                avatarImageUrl || "/aws-educate-avatar.png"
-              }" alt="Avatar of AWS Educate">
-              <div class="avatar-info">
-                  <p>${name}</p>
-                  <p>AWS Educate Cloud Ambassador</p>
-                  <p><a href="mailto:${email}">${email}</a></p>
-              </div>
-          </div>
-      </div>
-  </body>
-  </html>  
-  `;
+    <!DOCTYPE html>
+    <html lang="zh-TW">
+    <head>
+        <meta charset="UTF-8">
+        <title>${title}</title>
+    </head>
+    <body style="font-family: 'Arial', sans-serif; background-color: #f4f4f9; margin: 0; padding: 20px;">
+        <div style="background-color: white; padding: 40px 60px 60px 60px; margin: auto; max-width: 800px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+            <img class="banner" src="${
+              bannerImageUrl || "/aws-educate-banner.png"
+            }" alt="Banner of AWS Educate" style="width: 100%; margin-bottom: 10px; border-radius: 8px;">
+            <p>親愛的{name} ，</p>
+            <div>
+                <p>您好！</p>
+                <p>恭喜您成功加入<strong style="color: #FB2200; font-weight: bold;">「${title}」</strong>！非常高興您決定與我們一起踏上學習的旅程，共同探索雲端技術的無限可能！</p>
+                <p>為確保您能夠有效利用本計畫資源，校園大使團隊精心統整許多考照資源於在 Notion Page，並建立 Discord 社群以促進更深入的討論和即時互動，讓您可以輕鬆地提問並與大使互動問答。</p>
+                <b>您可以透過以下連結訪問：</b>
+                <ul style="padding-left: 20px;">
+                  <li>Notion Page：<a target="_blank" href="https://aws-educate-tw.notion.site/AWS-0824fda6e4aa470e863c4d91daf9563a" style="color: #0077cc;">點擊這裡</a></li>
+                  <li>Discord 社群：{Discord Link}</li>
+                </ul>
+            </div>
+            <b>【注意事項】</b>
+            <ul style="padding-left: 20px;">
+                <li>請記得定期追蹤計畫 Notion Page 和 Discord 社群，我們會定期更新資源和消息。</li>
+                <li>陪跑計畫包含證照課程與獎勵課程，獎勵課程需完成規定的課程進度才可以參與，詳細規則請見 Notion Page。</li>
+            </ul>
+            <b>【開幕活動與報名資訊】</b>
+            <div>
+                <p>隨著「${title}」正式啟動，${content}</p>
+                <p>有意願參加活動請填寫以下報名表單，<span style="color: red;">表單將於 ${deadline} 截止</span>：<a target="_blank" href="${eventLink}" style="color: #0077cc;">點擊這裡</a></p>
+                <p>最後，我們再次感謝您的參與。這是一場充滿學習和成長的旅程，我們期待在開幕活動上見到您！</p>
+                <p>若您有任何問題或需要進一步的幫助，請聯繫我們。</p>
+                <p>台灣 AWS Educate Cloud Ambassador 官方社群：<a target="_blank" href="https://bit.ly/3pD9aCr" style="color: #0077cc;">Facebook</a>＆<a target="_blank" href="https://bit.ly/3BBr7XQ" style="color: #0077cc;">Instagram</a></p>
+            </div>
+            <br>
+                    
+            <p>Best regards,</p>
+            <div class="footer" style="display: flex; align-items: center; text-align: start; font-size: 0.9em; color: #777;">
+                <img class="avatar" src="${
+                  avatarImageUrl || "/aws-educate-avatar.png"
+                }" alt="Avatar of AWS Educate" style="width: 80px; height: 80px; border-radius: 50%; border: solid 2px #333; margin-right: 15px;">
+                <div class="avatar-info" style="display: flex; flex-direction: column; line-height: 0.5;">
+                    <p>${name}</p>
+                    <p>AWS Educate Cloud Ambassador</p>
+                    <p><a href="mailto:${email}" style="color: #0077cc;">${email}</a></p>
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>  
+    `;
 
   const handlePreview = async () => {
     const bannerBase64 = bannerImage
