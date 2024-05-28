6cd1c3ca18017c7723097199df4f9d1ff7337d0a
chungchihhan <harryup2000@gmail.com>
2024-05-28 17:06:55 +0800

    style:change the ui of the input file

--- src/app/ui/file-upload.tsx
+++ src/app/ui/file-upload.tsx
@@ -1,8 +1,7 @@
 "use client";
 import React, { useRef, useState, FormEvent } from "react";
-import { set } from "zod";
 
-const UploadFile: React.FC = () => {
+export default function UploadFile() {
   const ref = useRef<HTMLFormElement>(null);
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const [link, setLink] = useState<string>("");
@@ -50,16 +49,35 @@ const UploadFile: React.FC = () => {
   return (
     <>
       <form onSubmit={onSubmit} ref={ref}>
-        <input type="file" name="file" accept=".xlsx" disabled={isSubmitting} />
-        <button type="submit" disabled={isSubmitting}>
-          Upload File
-        </button>
+        <div className="rounded-md bg-gray-50 p-4">
+          <label className="mb-2 block text-lg font-medium">
+            Upload a xlsx file
+          </label>
+          <div className="flex justify-between py-3 px-5 gap-3 items-center bg-white rounded-md border border-gray-200 text-sm outline-2 placeholder:text-gray-500 w-full">
+            <input
+              type="file"
+              name="file"
+              accept=".xlsx"
+              disabled={isSubmitting}
+              className="flex flex-grow hover:cursor-pointer text-lg"
+            />
+            <button
+              type="submit"
+              className={`flex h-10 items-center rounded-lg ${
+                isSubmitting ? "bg-gray-500" : "bg-sky-800 hover:bg-sky-700"
+              } px-4 text-sm font-medium text-white transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-800 active:bg-sky-950 aria-disabled:cursor-not-allowed aria-disabled:opacity-50`}
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Uploading..." : "Upload File"}
+            </button>
+          </div>
+        </div>
       </form>
-      <a href={link} className="hover:text-blue-500 hover:underline">
-        This is the S3 url you have uploaded
-      </a>
+      {link && (
+        <a href={link} className="hover:text-blue-500 hover:underline">
+          This is the S3 url you have uploaded
+        </a>
+      )}
     </>
   );
-};
-
-export default UploadFile;
+}
