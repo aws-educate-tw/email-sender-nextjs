3d11a1a38406ba4cbdad465208c2006e3d576040
chungchihhan <harryup2000@gmail.com>
2024-06-17 22:12:55 +0800

    feat: add other functions of rich editor | SCRUM-11

--- src/app/ui/Toolbar.tsx
+++ src/app/ui/Toolbar.tsx
@@ -11,7 +11,6 @@ import {
   Quote,
   Undo,
   Redo,
-  Code,
 } from "lucide-react";
 
 type Props = {
@@ -36,6 +35,124 @@ const Toolbar = ({ editor, content }: Props) => {
           }
         >
           <Bold className="w-5 h-5" />
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            editor.chain().focus().toggleItalic().run();
+          }}
+          className={
+            editor.isActive("italic")
+              ? "bg-sky-700 text-white p-1 rounded-lg"
+              : "text-sky-400 p-1"
+          }
+        >
+          <Italic className="w-5 h-5" />
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            editor.chain().focus().toggleUnderline().run();
+          }}
+          className={
+            editor.isActive("underline")
+              ? "bg-sky-700 text-white p-1 rounded-lg"
+              : "text-sky-400 p-1"
+          }
+        >
+          <Underline className="w-5 h-5" />
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            editor.chain().focus().toggleStrike().run();
+          }}
+          className={
+            editor.isActive("strike")
+              ? "bg-sky-700 text-white p-1 rounded-lg"
+              : "text-sky-400 p-1"
+          }
+        >
+          <Strikethrough className="w-5 h-5" />
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            editor.chain().focus().toggleHeading({ level: 2 }).run();
+          }}
+          className={
+            editor.isActive("heading", { level: 2 })
+              ? "bg-sky-700 text-white p-1 rounded-lg"
+              : "text-sky-400 p-1"
+          }
+        >
+          <Heading2 className="w-5 h-5" />
+        </button>
+
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            editor.chain().focus().toggleBulletList().run();
+          }}
+          className={
+            editor.isActive("bulletList")
+              ? "bg-sky-700 text-white p-1 rounded-lg"
+              : "text-sky-400 p-1"
+          }
+        >
+          <List className="w-5 h-5" />
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            editor.chain().focus().toggleOrderedList().run();
+          }}
+          className={
+            editor.isActive("orderedList")
+              ? "bg-sky-700 text-white p-1 rounded-lg"
+              : "text-sky-400 p-1"
+          }
+        >
+          <ListOrdered className="w-5 h-5" />
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            editor.chain().focus().toggleBlockquote().run();
+          }}
+          className={
+            editor.isActive("blockquote")
+              ? "bg-sky-700 text-white p-1 rounded-lg"
+              : "text-sky-400 p-1"
+          }
+        >
+          <Quote className="w-5 h-5" />
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            editor.chain().focus().undo().run();
+          }}
+          className={
+            editor.isActive("undo")
+              ? "bg-sky-700 text-white p-1 rounded-lg"
+              : "text-sky-400 hover:bg-sky-700 hover:text-white p-1 hover:rounded-lg"
+          }
+        >
+          <Undo className="w-5 h-5" />
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            editor.chain().focus().redo().run();
+          }}
+          className={
+            editor.isActive("redo")
+              ? "bg-sky-700 text-white p-1 rounded-lg"
+              : "text-sky-400 hover:bg-sky-700 hover:text-white p-1 hover:rounded-lg"
+          }
+        >
+          <Redo className="w-5 h-5" />
         </button>
       </div>
     </div>
