f35f49d699abe962c1ce066bbc5c06e346255533
chungchihhan <harryup2000@gmail.com>
2024-06-20 23:24:58 +0800

    style: add a loading while fetching files | SCRUM-41

--- src/app/ui/select-dropdown.tsx
+++ src/app/ui/select-dropdown.tsx
@@ -31,6 +31,7 @@ export default function SelectDropdown({
   );
   const [searchTerm, setSearchTerm] = useState("");
   const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedFileName, setSelectedFileName] = useState(
     `Select a ${fileExtension} file`
   );
@@ -63,6 +64,7 @@ export default function SelectDropdown({
 
   const fetchFiles = async (file_extension: string, limit: number) => {
     try {
+      setIsLoading(true);
       const base_url =
         "https://8um2zizr80.execute-api.ap-northeast-1.amazonaws.com/dev";
       const url = new URL(`${base_url}/files`);
@@ -82,6 +84,8 @@ export default function SelectDropdown({
       setTemplateOptions(result.data);
     } catch (error: any) {
       alert("Failed to fetch files: " + error.message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -145,7 +149,31 @@ export default function SelectDropdown({
             />
           </div>
 
-          {filteredOptions && filteredOptions.length > 0 ? (
+          {isLoading ? (
+            <div className="flex justify-center items-center py-4">
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-500"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291l-1.497-1.32A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              <p>Loading...</p>
+            </div>
+          ) : filteredOptions && filteredOptions.length > 0 ? (
             <div>
               <table className="w-full bg-white">
                 <thead>
