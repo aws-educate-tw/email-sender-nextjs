4508a1ca5cc97741c11414d3c8c6e79e1ed32335
chungchihhan <harryup2000@gmail.com>
2024-06-25 14:13:47 +0800

    fix: remove the button on sidenav | SCRUM-41

--- src/app/ui/side-nav.tsx
+++ src/app/ui/side-nav.tsx
@@ -19,12 +19,12 @@ export default function SideNav() {
         />
       </Link>
       <div className="flex grow flex-row flex-wrap justify-start gap-2 md:flex-col md:space-x-0 rounded-md">
-        <Link
+        {/* <Link
           href="/listFiles"
           className="flex flex-grow min-w-48 max-h-10 items-center justify-center rounded-md bg-sky-950 p-4 hover:bg-sky-800"
         >
           <p className="px-3 text-white">TPET drive</p>
-        </Link>
+        </Link> */}
         <Link
           href="/sendEmail"
           className="flex flex-grow min-w-48 max-h-10 items-center justify-center rounded-md bg-sky-950 p-4 hover:bg-sky-800"
