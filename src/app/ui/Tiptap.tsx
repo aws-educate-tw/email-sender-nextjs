5dd4d957b01466a45fd55bd246b0fe2192277735
chungchihhan <harryup2000@gmail.com>
2024-07-16 13:43:23 +0800

    fix: fix the preview of html template | SCRUM-11

--- src/app/ui/Tiptap.tsx
+++ src/app/ui/Tiptap.tsx
@@ -13,7 +13,7 @@ import ImageResize from "tiptap-extension-resize-image";
 import { useState } from "react";
 import NextStepLink from "next/link";
 
-const htmltemplate = `
+const htmltemplateContent = `
     <p>親愛的{{Name}}，</p>
     <p>您好！恭喜您成功加入<strong class="highlight">「6th AWS Educate Taiwan 雲端校園大使證照陪跑計畫」</strong>！非常高興您決定與我們一起踏上學習的旅程，共同探索雲端技術的無限可能！</p>
     <p>為確保您能夠有效利用本計畫資源，校園大使團隊精心統整許多考照資源於在 Notion Page，並建立 Discord 社群以促進更深入的討論和即時互動，讓您可以輕鬆地提問並與大使互動問答。</p>
@@ -28,13 +28,24 @@ const htmltemplate = `
     <p>billwu0222@gmail.com</p>`;
 
 const Tiptap = ({ onChange, content }: any) => {
-  const [editorContent, setEditorContent] = useState(htmltemplate);
+  const [editorContent, setEditorContent] = useState(htmltemplateContent);
   const [isFocused, setIsFocused] = useState(false);
   const [isUploading, setIsUploading] = useState(false);
   const [showNextStep, setShowNextStep] = useState(false);
 
   const handleUpload = async () => {
-    const blob = new Blob([editorContent], { type: "text/html" });
+    const html = `
+    <!DOCTYPE html>
+    <html lang="zh-TW">
+    <head>
+        <meta charset="UTF-8">
+        <title>加入 AWS Educate Taiwan 雲端校園大使證照陪跑計畫</title>
+    </head>
+    <body>
+        ${editorContent}
+    </body>
+    </html>`;
+    const blob = new Blob([html], { type: "text/html" });
     const formData = new FormData();
     formData.append("file", blob, "html-generate.html");
 
@@ -81,7 +92,7 @@ const Tiptap = ({ onChange, content }: any) => {
           "w-full flex-col justify-start text-black items-start w-full gap-3 pt-4 rounded-bl-md rounded-br-md outline-none",
       },
     },
-    content: htmltemplate,
+    content: htmltemplateContent,
     onUpdate: ({ editor }) => {
       handleChange(editor.getHTML());
     },
