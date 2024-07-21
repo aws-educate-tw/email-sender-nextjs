03cd1050199043487f65e9dc2331668ed7e1cc90
chungchihhan <harryup2000@gmail.com>
2024-07-21 21:01:50 +0800

    feat: connect the change password api | SCRUM-57

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { FormEvent, useRef } from "react";
-import { submitLogin } from "@/lib/actions";
+import { FormEvent, useRef, useState, useEffect } from "react";
+import { submitLogin, submitChangePassword } from "@/lib/actions";
 
 interface SubmitResponse {
   message: string;
@@ -14,10 +14,27 @@ interface SubmitResponse {
 }
 
 export default function Page() {
+  const [session, setSession] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [newPassword, setNewPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false); // for the pw of login and the pw of new password
+  const [showConfirmPassword, setShowConfirmPassword] =
+    useState<boolean>(false); //for the confirm pw
   const ref = useRef<HTMLFormElement>(null);
+
+  useEffect(() => {
+    if (newPassword !== confirmPassword) {
+      setError("Passwords do not match");
+    } else {
+      setError(null);
+    }
+  }, [newPassword, confirmPassword]);
+
   const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (!ref.current) return;
+    setShowPassword(false);
 
     const formData = {
       account: (ref.current.querySelector("[id='account']") as HTMLInputElement)
@@ -40,15 +57,47 @@ export default function Page() {
       alert(response.message);
 
       if (response.challengeName == "NEW_PASSWORD_REQUIRED") {
-        console.log("New password required");
+        console.log(response);
+        setSession(response.session);
       }
     } catch (error: any) {
       console.error("Login failed", error);
     }
   };
+
+  const onSubmitNewPassword = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (newPassword !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+    if (!ref.current) return;
+
+    const account = (
+      ref.current.querySelector("[id='account']") as HTMLInputElement
+    ).value;
+
+    const payload = {
+      account: account,
+      new_password: newPassword,
+      session: session,
+    };
+
+    try {
+      const response = await submitChangePassword(JSON.stringify(payload));
+      alert("Password changed successfully");
+    } catch (error: any) {
+      console.error("Password change failed", error);
+    }
+  };
+
   return (
     <div className="flex h-screen justify-center items-center">
-      <form onSubmit={onSubmit} ref={ref} className="max-w-80">
+      <form
+        onSubmit={session ? onSubmitNewPassword : onSubmit}
+        ref={ref}
+        className="max-w-80"
+      >
         <div className="rounded-md bg-neutral-100 p-4">
           <div className="m-3">
             <label className="mb-2 block text-sm font-medium">Username</label>
@@ -61,23 +110,78 @@ export default function Page() {
             />
           </div>
 
-          <div className="m-3">
-            <label className="mb-2 block text-sm font-medium">Password</label>
-            <input
-              id="password"
-              name="password"
-              type="text"
-              placeholder="Enter the password"
-              className="block rounded-md border py-2 pl-4 text-sm outline-2 placeholder:text-gray-500 w-full"
-            />
-          </div>
+          {!session && (
+            <div className="m-3 relative">
+              <label className="mb-2 block text-sm font-medium">Password</label>
+              <input
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter the password"
+                className="block rounded-md border py-2 pl-4 text-sm outline-2 placeholder:text-gray-500 w-full"
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-2 top-12 transform -translate-y-1/2 bg-gray-200 rounded-md px-2 py-1 text-sm"
+              >
+                {showPassword ? "Hide" : "Show"}
+              </button>
+            </div>
+          )}
+
+          {session && (
+            <>
+              <div className="m-3 relative">
+                <label className="mb-2 block text-sm font-medium">
+                  New Password
+                </label>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Enter new password"
+                  className="block rounded-md border py-2 pl-4 text-sm outline-2 placeholder:text-gray-500 w-full"
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-2 top-12 transform -translate-y-1/2 bg-gray-200 rounded-md px-2 py-1 text-sm"
+                >
+                  {showPassword ? "Hide" : "Show"}
+                </button>
+              </div>
+
+              <div className="m-3 relative">
+                <label className="mb-2 block text-sm font-medium">
+                  Confirm New Password
+                </label>
+                <input
+                  type={showConfirmPassword ? "text" : "password"}
+                  placeholder="Confirm new password"
+                  className="block rounded-md border py-2 pl-4 text-sm outline-2 placeholder:text-gray-500 w-full"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                  className="absolute right-2 top-12 transform -translate-y-1/2 bg-gray-200 rounded-md px-2 py-1 text-sm"
+                >
+                  {showConfirmPassword ? "Hide" : "Show"}
+                </button>
+              </div>
+
+              {error && <div className="text-red-500 text-sm">{error}</div>}
+            </>
+          )}
         </div>
         <div className="w-full flex justify-end my-3 gap-3">
           <button
             type="submit"
             className="flex h-10 items-center rounded-lg bg-sky-950 hover:bg-sky-800 px-4 md:text-base text-xs font-medium text-white transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-800 active:bg-sky-950"
           >
-            login
+            {session ? "Change Password" : "Login"}
           </button>
         </div>
       </form>
