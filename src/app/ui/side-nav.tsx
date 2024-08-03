e7227556d7e43f8b0e777576e551be6491da8ba2
chungchihhan <harryup2000@gmail.com>
2024-08-03 20:18:43 +0800

    feat: add a navigation in the sidenav | SCRUM-9

--- src/app/ui/side-nav.tsx
+++ src/app/ui/side-nav.tsx
@@ -39,6 +39,12 @@ export default function SideNav() {
         >
           <p className="px-3 text-white">Send Email</p>
         </Link>
+        <Link
+          href="/emailHistory"
+          className="flex flex-grow min-w-48 max-h-10 items-center justify-center rounded-md bg-sky-950 p-4 hover:bg-sky-800"
+        >
+          <p className="px-3 text-white">Email History</p>
+        </Link>
       </div>
       <div>
         <button
