60820de2a4932b7c854ee37023a485ba506330d1
poyang1024 <poyang1024@gmail.com>
2025-01-08 01:56:16 +0800

    feat: add link to Webhook Records in side navigation | SCRUM-161

--- src/app/ui/side-nav.tsx
+++ src/app/ui/side-nav.tsx
@@ -57,6 +57,12 @@ export default function SideNav() {
         >
           <p className="px-3 text-white">Webhook Service</p>
         </Link>
+        <Link
+          href="/webhookRecords"
+          className="flex flex-grow min-w-48 max-h-10 items-center justify-center rounded-md bg-sky-950 p-4 hover:bg-sky-800"
+        >
+          <p className="px-3 text-white">Webhook Records</p>
+        </Link>
       </div>
       <div>
         <button
