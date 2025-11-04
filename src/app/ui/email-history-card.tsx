0c4397e8293e9a3d75c917a24181330ca6ba728b
chungchihhan <harryup2000@gmail.com>
2025-11-04 20:54:53 +0800

    style: style the email history card | SCRUM-404

--- src/app/ui/email-history-card.tsx
+++ src/app/ui/email-history-card.tsx
@@ -107,7 +107,7 @@ export default function EmailHistoryCard({ data }: PropsType) {
         <Link
           key={item.run_id}
           href={`/emailHistory/${item.run_id}`}
-          className="w-full hover:shadow-xl transition bg-white rounded-xl shadow-md overflow-hidden"
+          className="w-full hover:shadow-xl transition rounded-xl shadow-md overflow-hidden bg-white"
         >
           <div className="p-8">
             <div className="flex flex-col">
@@ -115,7 +115,7 @@ export default function EmailHistoryCard({ data }: PropsType) {
                 {item.subject ? item.subject : "No Subject"}
               </div>
               <hr />
-              <div className="flex flex-col lg:flex-row justify-between">
+              <div className="flex flex-col py-2 xl:gap-4 xl:flex-row justify-between">
                 {/* Sender Info */}
                 <div className="flex-1 flex py-2 gap-2 items-center">
                   <div className="bg-sky-900 rounded-full p-1 border-2 border-sky-900">
@@ -149,33 +149,41 @@ export default function EmailHistoryCard({ data }: PropsType) {
               </div>
 
               {/* Footer Info */}
-              <div className="flex flex-col justify-end pt-2 pl-3 gap-2 lg:flex-row lg:pt-8 lg:gap-8">
+              <div className="flex flex-col justify-end pt-2 pl-3 gap-2 lg:flex-row lg:pt-8 lg:gap-8 text-black">
                 <div className="flex items-center gap-2">
-                  <div className="flex items-center gap-2">
+                  <div className="flex items-center gap-2 lg:min-w-20 min-w-24">
                     <CalendarDays size={20} />
                     <p>Send at</p>
                   </div>
-                  <strong>{convertToTaipeiTime(item.created_at)}</strong>
+                  <p className="underline">
+                    {/* Convert to Taipei Timezone */}
+                    <strong>{convertToTaipeiTime(item.created_at)}</strong>
+                  </p>
                 </div>
 
                 <div className="flex items-center gap-2">
-                  <div className="flex items-center gap-2">
+                  <div className="flex items-center gap-2 lg:min-w-20 min-w-24">
                     <Send size={20} />
                     <p>Sender</p>
                   </div>
-                  <strong>{item.sender?.username || "Unknown"}</strong>
+                  <a
+                    href={`mailto:${item.sender.email}`}
+                    className="text-black underline hover:text-sky-800"
+                  >
+                    <strong>{item.sender.username || "Unknown"}</strong>
+                  </a>
                 </div>
 
                 <div className="flex items-center gap-2">
-                  <div className="flex items-center gap-2">
+                  <div className="flex items-center gap-2 lg:min-w-20 min-w-24">
                     <Clock size={20} />
                     <p>Status</p>
                   </div>
                   <strong
                     className={
                       item.success_email_count < item.expected_email_send_count
-                        ? "text-red-500"
-                        : "text-green-500"
+                        ? "text-red-500 text-2xl"
+                        : "text-green-500 text-2xl"
                     }
                   >
                     {item.success_email_count}/{item.expected_email_send_count}
